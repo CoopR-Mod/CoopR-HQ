726faf5185fa170eda781f24d80f59f8c6bc453d
xetra11 <xetra11@outlook.com>
2018-11-12 00:04:02 +0100

    init(project): init commit

---
+++ src/main/java/com/coopr/hq/CooprHqApplication.java
@@ -0,0 +1,12 @@
+package com.coopr.hq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooprHqApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CooprHqApplication.class, args);
+  }
+}
