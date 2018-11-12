a42c395670e95c7852a3dd8a5ca6690b573ae373
xetra11 <xetra11@outlook.com>
2018-11-13 00:05:39 +0100

    feature(endpoint): add first iteration of persistence endpoints
    COOPR-53

--- src/main/java/com/coopr/hq/endpoints/HqEndpoint.java
+++ src/main/java/com/coopr/hq/endpoints/HqEndpoint.java
@@ -1,7 +1,16 @@
 package com.coopr.hq.endpoints;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.coopr.hq.models.Character;
+import com.coopr.hq.models.Person;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.logging.Level;
 
 /***************************************
  * Author: xetra11                     
@@ -11,13 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
  * API: -                              
  **************************************/
 @RestController
+@Log
 public class HqEndpoint {
 
- @GetMapping("/test")
- public String test(){
-   return "success";
- }
+  private MongoTemplate mongoTemplate;
 
+  @Autowired
+  public HqEndpoint(MongoTemplate mongoTemplate) {
+    this.mongoTemplate = mongoTemplate;
+  }
 
+  @PostMapping(value = "/save/characters/", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void updateCharacters(@RequestBody List<Character> characters) {
+    mongoTemplate.save(characters);
+    log.log(Level.INFO, "{} characters have been saved", characters.size());
+  }
+
+  @PostMapping(value = "/save/character/", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void updateCharacter(@RequestBody Character character) {
+    mongoTemplate.save(character);
+    log.log(Level.INFO, "character with uid {} has been saved", character.getUid());
+  }
+
+  @GetMapping("/fetch/characters/")
+  public List<Character> fetchCharacter() {
+    return mongoTemplate.findAll(Character.class);
+  }
 
 }
